544d29ab65b6ce59529ab57eb7d1583ea52043f9
nexxbase2025 <donbeltranmusic@gmail.com>
2025-07-11 16:49:53 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('nautica-gym').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
