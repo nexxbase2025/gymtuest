d60a6efeb091cf62e2545df9fb2456b83b932ecd
nexxbase2025 <donbeltranmusic@gmail.com>
2025-07-12 00:34:30 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,42 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('nautica-gym').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/manifest.json'
-      ]);
+
+const CACHE_NAME = 'nautica-gym-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/inscribete.html',
+  '/servicios.html',
+  '/galeria.html',
+  '/style.css',
+  '/manifest.json',
+  '/logo.png',
+  '/gym0.jpeg',
+  '/gym1.jpeg',
+  '/gym2.jpeg',
+  '/gym3.jpeg',
+  '/icongym.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
